b56483e82746975a29b8847495de9f77f9e32ef9
Marat-BY <maratmovlamov2017@gmail.com>
2021-07-01 14:36:23 +0300

    rabbitMQ functionality added

--- README.md
+++ README.md
@@ -18,19 +18,28 @@ This will install the module to your environment.
 
 To begin monitor server changes run the command:
 
-`
-$ python3 notifier.py --monitor_dir'./directory_to_monitor/' --logs_dir './directory_to_output_logs/logs.log'
-`
+
+~~~
+$python3 notifier.py --monitor_dir './trash/' --logs_dir './logs.log'  --usr "UserName" --pwd "Password" --env "Environment"
+~~~
 
 The example of the logs file:
 
 ~~~
-2021-06-29 15:14:20<Event dir=True mask=0x40000020 maskname=IN_OPEN|IN_ISDIR name='' path=tests pathname=/home/user/python_projects/messaging/tests wd=1 >
-2021-06-29 15:14:20<Event dir=True mask=0x40000001 maskname=IN_ACCESS|IN_ISDIR name='' path=tests pathname=/home/user/python_projects/messaging/tests wd=1 >
-2021-06-29 15:14:20<Event dir=True mask=0x40000010 maskname=IN_CLOSE_NOWRITE|IN_ISDIR name='' path=tests pathname=/home/user/python_projects/messaging/tests wd=1 >
-2021-06-29 15:14:28<Event dir=True mask=0x40000020 maskname=IN_OPEN|IN_ISDIR name='' path=tests pathname=/home/user/python_projects/messaging/tests wd=1 >
-2021-06-29 15:14:28<Event dir=True mask=0x40000001 maskname=IN_ACCESS|IN_ISDIR name='' path=tests pathname=/home/user/python_projects/messaging/tests wd=1 >
-2021-06-29 15:14:28<Event dir=True mask=0x40000010 maskname=IN_CLOSE_NOWRITE|IN_ISDIR name='' path=tests pathname=/home/user/python_projects/messaging/tests wd=1 >
+2021-07-01T10:59:39Z	IN_MODIFY	test.csv	/home/user/python_projects/messaging/trash/test.csv
+2021-07-01T10:59:48Z	IN_CREATE	queue.txt	/home/user/python_projects/messaging/trash/queue.txt
+2021-07-01T10:59:48Z	IN_MODIFY	queue.txt	/home/user/python_projects/messaging/trash/queue.txt
+2021-07-01T11:00:04Z	IN_CREATE|IN_ISDIR	pathes	/home/user/python_projects/messaging/trash/pathes
+2021-07-01T11:00:51Z	IN_DELETE|IN_ISDIR	tests_monitor	/home/user/python_projects/messaging/trash/tests_monitor
+2021-07-01T11:00:51Z	IN_DELETE_SELF		/home/user/python_projects/messaging/trash/tests_monitor
+2021-07-01T11:00:51Z	IN_IGNORED		/home/user/python_projects/messaging/trash/tests_monitor
+2021-07-01T11:00:55Z	IN_DELETE	test.csv	/home/user/python_projects/messaging/trash/test.csv
+2021-07-01T11:00:59Z	IN_DELETE	queue.txt	/home/user/python_projects/messaging/trash/queue.txt
+2021-07-01T11:01:02Z	IN_DELETE|IN_ISDIR	pathes	/home/user/python_projects/messaging/trash/pathes
+2021-07-01T11:01:02Z	IN_DELETE_SELF		/home/user/python_projects/messaging/trash/pathes
+2021-07-01T11:01:02Z	IN_IGNORED		/home/user/python_projects/messaging/trash/pathes
+2021-07-01T11:11:01Z	IN_MODIFY	test.csv	/home/user/python_projects/messaging/trash/test.csv
+
 ~~~~
 
 
--- notifier.py
+++ notifier.py
@@ -1,22 +1,28 @@
 import pyinotify
-from datetime import datetime
+from datetime import datetime, tzinfo, timedelta
 import argparse
+import pika
 import warnings
 warnings.filterwarnings("ignore")
 
 
 class Log(pyinotify.ProcessEvent):
-    def my_init(self, fileobj):
-        """
-        Method automatically called from ProcessEvent.__init__(). Additional
-        keyworded arguments passed to ProcessEvent.__init__() are then
-        delegated to my_init(). This is the case for fileobj.
-        """
+    def my_init(self, fileobj, channel):
         self._fileobj = fileobj
+        self._channel = channel
 
     def process_default(self, event):
-        self._fileobj.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '\t' + str(event) + '\n')
+        message = str(str(datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")) + \
+                      '\t' + str(event.maskname) + '\t' + str(event.name)+ \
+                      '\t'  + str(event.pathname) + '\n')
+        message_to_mq = str({"createdAt" : str(datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")),
+                         "eventName" : str(event.maskname),
+                         "file" : str(event.name),
+                         "fullPath": str(event.pathname)})
+        self._channel.basic_publish(exchange='', routing_key='NasMonitor', body=message_to_mq)
+        self._fileobj.write(message)
         self._fileobj.flush()
+
 
 class TrackModifications(pyinotify.ProcessEvent):
     def process_IN_MODIFY(self, event):
@@ -37,27 +43,50 @@ def parse_cli_arguments():
     parser.add_argument(u"--monitor_dir", required=True, type=str, help="Directory to watch, e.g. -> './testdir/' ")
     parser.add_argument(u"--logs_dir", required=False, type=str, default='./pyinotify_moonbug_NAS_log.log',
                         help="Directory to save logs, e.g. -> './logs/logs.log/' ")
+    parser.add_argument(u"--usr", required=True, type=str, help="Username for RabbitMQ account")
+    parser.add_argument(u"--pwd", required=True, type=str, help="Password for RabbitMQ account")
+    parser.add_argument(u"--env", required=True, type=str, help="Environment for RabbitMQ service")
     return parser.parse_args()
 
 
 def monitor():
+    # pyinotify:
     parsed_cli_arguments = parse_cli_arguments()
     monitor_dir = parsed_cli_arguments.monitor_dir
-    dir_logs = parsed_cli_arguments.logs_dir
+    logs_dir = parsed_cli_arguments.logs_dir
 
-    logs_outputs = open(dir_logs, "a")
+    # RabbitMQ Credentials
+    rabbit_host = "35.234.130.164"
+    rabbit_port = 5672
+    rabbit_username = parsed_cli_arguments.usr
+    rabbit_password = parsed_cli_arguments.pwd
+    rabbit_env = parsed_cli_arguments.env
+
+    logs_outputs = open(logs_dir, "a")
+
+    rabbit_params = pika.ConnectionParameters(host=rabbit_host, port=rabbit_port, virtual_host= rabbit_env,
+                                              credentials=pika.PlainCredentials(username=rabbit_username,
+                                                                                password=rabbit_password))
+    rabbit_connection = pika.BlockingConnection(rabbit_params)
 
     try:
+        # Rabbit MQ messaging
+        rabbit_channel = rabbit_connection.channel()
+        rabbit_channel.queue_declare(queue='NasMonitor', durable=True)
+
+        # Messaging Notifier
         watch_manager = pyinotify.WatchManager()
-        mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY
-        handler = Empty(TrackModifications(Log(fileobj=logs_outputs)), msg = f"Logs updated {datetime.now()}")
+        mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY | pyinotify.IN_DELETE_SELF | pyinotify.IN_MOVE_SELF | pyinotify.IN_MOVED_FROM | pyinotify.IN_MOVED_TO
+        handler = Empty(TrackModifications(Log(fileobj=logs_outputs, channel=rabbit_channel)), msg = "Logs updated.")
         notifier = pyinotify.Notifier(watch_manager, default_proc_fun=handler)
-        watch_manager.add_watch(monitor_dir, mask=mask, rec=True)
+        watch_manager.add_watch(monitor_dir, mask=mask, rec=True, auto_add=True)
         notifier.loop()
     except KeyboardInterrupt:
         logs_outputs.close()
+        rabbit_connection.close()
     finally:
         logs_outputs.close()
+        rabbit_connection.close()
 
 
 if __name__ == "__main__":
